8a5fc24cc6a8213813f187bf830fd3a37e954e82
Anjali <anjalimandal5701@gmail.com>
2026-01-20 16:15:57 +0530

    Fix case-sensitive CSS import

--- frontend/src/views/home/Home.jsx
+++ frontend/src/views/home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { useAuth } from '../../contexts/AuthContext'
-import './Home.css'
+import './home.css'
 
 const Home = () => {
 
